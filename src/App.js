e9da53b80da86723f3d8378f63d0301ef350f6cf
marsalxavi <marsalxavi@gmail.com>
2021-06-22 18:24:47 +0200

    S7N02Ex02

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 import textosTeatre from "./textosTeatre.json"
+import styled from 'styled-components';
+
+
+const StyledP = styled.p`
+  color:yellow;
+  background blue;
+  max-width: 40 rem;
+  margin: 5 rem auto;
+  padding; 5 rem auto;
+  font-size: 18 px;
+  font-style:italic;
+  `;
 
 function App() {
   const listTeatre = textosTeatre.map
     (
-      teatre => <p>{teatre}</p>
+      teatre => <StyledP>{teatre}</StyledP>
     )
-    
+
   return (
     <div>
       {listTeatre}
