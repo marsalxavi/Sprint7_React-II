ccf131198ee47aa5a890838f35dbed5846f12909
marsalxavi <marsalxavi@gmail.com>
2021-06-29 12:50:20 +0200

    S7N03Ex03

--- src/App.js
+++ src/App.js
@@ -2,30 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 
 import textosTeatre from "./textosTeatre.json"
-import styled from 'styled-components';
+import React from 'react';
 
+import StyledP from "./StyledP.js"
 
-const StyledP = styled.p`
-  color:yellow;
-  background blue;
-  max-width: 40 rem;
-  margin: 5 rem auto;
-  padding; 5 rem auto;
-  font-size: 18 px;
-  font-style:italic;
-  `;
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      numLinia: textosTeatre.length //init numLinia a linies text
+    }
+    this.lineUp = this.lineUp.bind(this)
+    this.lineDown = this.lineDown.bind(this)
+  }
 
-function App() {
-  const listTeatre = textosTeatre.map
-    (
-      teatre => <StyledP>{teatre}</StyledP>
+  lineUp() { //linia següent
+    this.setState(prevState => {
+      return {
+        numLinia: prevState.numLinia + 1
+      }
+    })
+  }
+
+  lineDown() { //linia anterior
+    this.setState(prevState => {
+      return {
+        numLinia: prevState.numLinia - 1
+      }
+    })
+  }
+
+  render() {
+    // El text sencer
+    const listTeatre = textosTeatre.map
+      (
+        teatre => <StyledP>{teatre}</StyledP>
+      )
+    // nomes una linia
+    const liniaTeatre =
+      (
+        <StyledP>{textosTeatre[this.state.numLinia - 1]}</StyledP>
+      )
+
+    return (
+      <div>
+        <button type="submit" onClick={this.lineDown} class="btn btn-primary">Linia Anterior</button>
+        <button type="submit" onClick={this.lineUp} class="btn btn-success">Linia Següent</button>
+        <p>Numero de línia: {this.state.numLinia}</p>
+        {liniaTeatre}
+      </div >
     )
-
-  return (
-    <div>
-      {listTeatre}
-    </div>
-  )
+  }
 }
 
 export default App;
